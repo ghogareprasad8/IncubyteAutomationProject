562c3f68763a203ddf8e1a9936d88469d5723e36
Prasad <ghogareprasad8@gmail.com>
2025-06-15 17:29:44 +0530

    Added screenshot capture after each step in Hooks using updated WebDriverContext class and saving in Results folder

--- IncubyteAutomation/Core/Hooks.cs
+++ IncubyteAutomation/Core/Hooks.cs
@@ -1,45 +1,21 @@
-// <copyright file="Hooks.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
+using System;
+using System.IO;
+using BoDi;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
 
 namespace IncubyteAutomation.Core
 {
-    using System;
-    using System.IO;
-    using AventStack.ExtentReports;
-    using AventStack.ExtentReports.Gherkin.Model;
-    using AventStack.ExtentReports.Reporter;
-    using BoDi;
-    using NUnit.Framework;
-    using OpenQA.Selenium;
-    using IncubyteAutomation.Core;
-    using TechTalk.SpecFlow;
-    using AventStack.ExtentReports.Reporter.Configuration;
-
-    /// <summary>
-    /// Hooks class.
-    /// </summary>
     [Binding]
     public class Hooks
     {
-        [ThreadStatic]
-        private static ExtentTest featureName;
-        [ThreadStatic]
-        private static ExtentTest scenario;
-        [ThreadStatic]
-        private static ExtentReports extent;
         private readonly IObjectContainer _objectContainer;
         private readonly ScenarioContext _scenarioContext;
         private readonly FeatureContext _featureContext;
         private readonly WebDriverContext _webDriverContext;
+        private int stepCounter =1;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Hooks"/> class.
-        /// </summary>
-        /// <param name="objectContainer">Object Container.</param>
-        /// <param name="featureContext">Feature Context.</param>
-        /// <param name="scenarioContext">Scenario Context.</param>
-        /// <param name="webDriverContext">Web Driver Context.</param>
         public Hooks(IObjectContainer objectContainer, FeatureContext featureContext, ScenarioContext scenarioContext, WebDriverContext webDriverContext)
         {
             this._objectContainer = objectContainer;
@@ -48,167 +24,57 @@ namespace IncubyteAutomation.Core
             this._webDriverContext = webDriverContext;
         }
 
-        /// <summary>
-        /// Before Test run method.
-        /// </summary>
         [BeforeTestRun]
         public static void BeforeTestRun()
         {
             Console.WriteLine("Before test run");
         }
 
-        /// <summary>
-        /// Statements to be executed before Feature.
-        /// </summary>
         [BeforeFeature]
         public static void BeforeFeature()
         {
             Console.WriteLine("Before feature run");
         }
 
-        /// <summary>
-        /// Statements to be executed after feature.
-        /// </summary>
         [AfterFeature]
         public static void AfterFeature()
         {
             Console.WriteLine("After feature run");
         }
 
-        /// <summary>
-        /// Statements to be executed after Test run.
-        /// </summary>
         [AfterTestRun]
         public static void AfterTestRun()
         {
             Console.WriteLine("After test run");
         }
 
-        // Update the BeforeScenario method
         [BeforeScenario]
         public void BeforeScenario()
         {
-            string downloadPath = $@"{AppDomain.CurrentDomain.BaseDirectory}\Downloads";
+            string downloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Downloads");
             this._objectContainer.RegisterInstanceAs(this._webDriverContext.driver, typeof(IWebDriver));
-
-            // Ensure Reusable is defined and accessible
-            Reusable.TestCaseName = this._scenarioContext.ScenarioInfo.Title; // Ensure Reusable is defined
-
-            string folderPath = $@"{AppDomain.CurrentDomain.BaseDirectory}\TestResults\{this._scenarioContext.ScenarioInfo.Title}\";
-            if (!Directory.Exists(folderPath))
-            {
-                Directory.CreateDirectory(folderPath);
-            }
 
             if (!Directory.Exists(downloadPath))
             {
                 Directory.CreateDirectory(downloadPath);
             }
 
-            string testReportPath = folderPath + "index.html";
-            this._scenarioContext.Add("extentRepoPath", testReportPath);
-            var htmlReporter = new ExtentHtmlReporter(this._scenarioContext.Get<string>("extentRepoPath"));
-            extent = new ExtentReports();
-            htmlReporter.Configuration().DocumentTitle = " Automation Report";
-            htmlReporter.Configuration().ReportName = " Automation Report";
-            htmlReporter.Configuration().Theme = Theme.Dark; // Use Theme directly if Configuration is not found
-            extent.AttachReporter(htmlReporter);
-            featureName = extent.CreateTest<Feature>(this._featureContext.FeatureInfo.Title);
-            scenario = featureName.CreateNode<Scenario>(this._scenarioContext.ScenarioInfo.Title);
+            Reusable.TestCaseName = this._scenarioContext.ScenarioInfo.Title;
         }
 
-        /// <summary>
-        /// Insert Specflow Reporting Steps.
-        /// </summary>
         [AfterStep]
-        public void InsertReportingSteps()
+        [AfterStep]
+        public void AfterEachStep()
         {
-            var stepType = this._scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
-            var stepInfo = this._scenarioContext.StepContext.StepInfo.Text;
-            var resultOfImplementation = this._scenarioContext.ScenarioExecutionStatus.ToString();
-            var status = TestContext.CurrentContext.Result.Outcome.Status;
-            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
-            if (this._scenarioContext.TestError == null && resultOfImplementation == "OK")
-            {
-                if (stepType == "Given")
-                {
-                    scenario.CreateNode<Given>(stepInfo).Pass(resultOfImplementation);
-                }
-                else if (stepType == "When")
-                {
-                    scenario.CreateNode<When>(stepInfo).Pass(resultOfImplementation);
-                }
-                else if (stepType == "Then")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Pass(resultOfImplementation);
-                }
-                else if (stepType == "And")
-                {
-                    scenario.CreateNode<And>(stepInfo).Pass(resultOfImplementation);
-                }
-                else if (stepType == "But")
-                {
-                    scenario.CreateNode<And>(stepInfo).Pass(resultOfImplementation);
-                }
-            }
-            else if (this._scenarioContext.TestError != null || status == NUnit.Framework.Interfaces.TestStatus.Failed)
-            {
-                string testError = this._scenarioContext.TestError.Message;
-                var screenshotMediaEntity = this._webDriverContext.CaptureScreenshotAndReturn(this._scenarioContext.ScenarioInfo.Title.Trim());
-
-                if (stepType == "Given")
-                {
-                    scenario.CreateNode<Given>(stepInfo).Fail(testError + stackTrace, screenshotMediaEntity);
-                }
-                else if (stepType == "When")
-                {
-                    scenario.CreateNode<When>(stepInfo).Fail(testError + stackTrace, screenshotMediaEntity);
-                }
-                else if (stepType == "Then")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Fail(testError + stackTrace, screenshotMediaEntity);
-                }
-                else if (stepType == "And")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Fail(testError + stackTrace, screenshotMediaEntity);
-                }
-                else if (stepType == "But")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Fail(testError + stackTrace, screenshotMediaEntity);
-                }
-            }
-            else if (resultOfImplementation == "StepDefinitionPending")
-            {
-                string errorMessage = "Step Definition is not implemented!";
-                if (stepType == "Given")
-                {
-                    scenario.CreateNode<Given>(stepInfo).Skip(errorMessage);
-                }
-                else if (stepType == "When")
-                {
-                    scenario.CreateNode<When>(stepInfo).Skip(errorMessage);
-                }
-                else if (stepType == "Then")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Skip(errorMessage);
-                }
-                else if (stepType == "But")
-                {
-                    scenario.CreateNode<Then>(stepInfo).Skip(errorMessage);
-                }
-            }
+            this._webDriverContext.CaptureScreenshot(_scenarioContext.ScenarioInfo.Title, stepCounter);
+            stepCounter++;
         }
 
-        /// <summary>
-        /// Statements to be executed after scenario.
-        /// </summary>
         [AfterScenario]
         public void AfterScenario()
         {
             this._webDriverContext.Dispose();
-            //extent.Flush();
-            //TestContext.AddTestAttachment(this._scenarioContext.Get<string>("extentRepoPath"));
-            Console.WriteLine("After scenraio run");
+            Console.WriteLine("After scenario run");
         }
     }
 }
--- IncubyteAutomation/Core/WebDriverContext.cs
+++ IncubyteAutomation/Core/WebDriverContext.cs
@@ -4,60 +4,102 @@
 
 namespace IncubyteAutomation.Core
 {
-    using AventStack.ExtentReports;
-    using NUnit.Framework;
     using OpenQA.Selenium;
+    using NUnit.Framework;
     using OpenQA.Selenium.Chrome;
     using System;
+    using System.IO;
 
     /// <summary>
-    /// Web Driver Context class.
+    /// Manages WebDriver instance and supports screenshot capture.
     /// </summary>
     public class WebDriverContext : IDisposable
     {
         /// <summary>
-        /// Gets or sets web Driver.
+        /// Gets or sets the Selenium WebDriver instance.
         /// </summary>
-        public IWebDriver driver { get; set; }
+        public IWebDriver driver { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WebDriverContext"/> class.
         /// </summary>
         public WebDriverContext()
         {
+            string downloadPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Downloads");
             ChromeOptions options = new ChromeOptions();
-            string downloadPath = $@"{AppDomain.CurrentDomain.BaseDirectory}\Downloads";
+
             options.AddUserProfilePreference("download.default_directory", downloadPath);
             options.AddUserProfilePreference("download.prompt_for_download", false);
             options.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
-            options.AddArgument("test-type");
-            options.AddArgument("-disable-extensions");
+
+            options.AddArgument("--disable-extensions");
             options.AddArgument("--disable-gpu");
-            options.AddArgument("start-maximized");
-            options.AddArgument("force-device-scale-factor=1.20");
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-notifications");
             options.AcceptInsecureCertificates = true;
-            options.LeaveBrowserRunning = true;
+
             this.driver = new ChromeDriver(options);
             this.driver.Manage().Window.Maximize();
+
             this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(double.Parse(TestContext.Parameters["ImplicitWait"]));
             this.driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(double.Parse(TestContext.Parameters["PageLoad"]));
+
+            Console.WriteLine("[WebDriverContext] ChromeDriver initialized.");
         }
+
         /// <summary>
-        /// To Capture Screenshot.
+        /// Captures a screenshot and saves it locally.
         /// </summary>
-        /// <param name="name">Screenshot name.</param>
-        /// <returns>Returns Sreenshot.</returns>
-        public MediaEntityModelProvider CaptureScreenshotAndReturn(string name)
+        /// <param name="scenarioName">The name used to save the screenshot file.</param>
+        /// <returns>The full path of the saved screenshot.</returns>
+        // WebDriverContext.cs
+        public string CaptureScreenshot(string scenarioName, int stepNumber)
         {
-            var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot().AsBase64EncodedString;
-            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot).Build();
+            try
+            {
+                // Get clean scenario name for folder
+                string safeScenarioName = string.Join("_", scenarioName.Split(Path.GetInvalidFileNameChars()));
+
+                // Folder path relative to base directory
+                string baseFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\magneto.Softwaretesting\Results\Screenshots", safeScenarioName));
+
+                if (!Directory.Exists(baseFolder))
+                {
+                    Directory.CreateDirectory(baseFolder);
+                }
+
+                // Define filename as Step1, Step2...
+                string fileName = $"Step{stepNumber}.png";
+                string fullPath = Path.Combine(baseFolder, fileName);
+
+                var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
+                File.WriteAllBytes(fullPath, screenshot.AsByteArray);
+
+                Console.WriteLine($"[Screenshot] Saved to: {fullPath}");
+                return fullPath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Screenshot] Failed: {ex.Message}");
+                return null;
+            }
         }
+
+
         /// <summary>
-        /// To close the driver.
+        /// Quits the WebDriver instance.
         /// </summary>
         public void Dispose()
         {
-            this.driver?.Quit();
+            try
+            {
+                this.driver?.Quit();
+                Console.WriteLine("[WebDriverContext] ChromeDriver quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[WebDriverContext] Error during driver quit: {ex.Message}");
+            }
         }
     }
 }
